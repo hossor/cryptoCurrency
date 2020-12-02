e7e066833c1b4db07082aae506b99b6054d36b63
Mohammadreza katebi <Katebi_mohammadreza@yahoo.com>
2020-12-02 22:01:41 +0330

    initial commitíµ³

--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -8,10 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Hello" , "Hello world")
-		Log.d("Hello,mmd" , "Hello my name is mmd")
-        Log.d("Hello,Hossor" , "Hello my name is Hossor")
-        Log.d("Hello,test" , "Hello my name is test")
+
 
 
     }
