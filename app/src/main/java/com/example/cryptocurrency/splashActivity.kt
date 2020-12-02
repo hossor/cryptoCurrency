b541dcdd48a9d05eb007d35fdc78cd61993b22a6
hossor <hossor79@gmail.com>
2020-12-03 00:30:17 +0330

    add Handle runable to splashScreen

--- app/src/main/java/com/example/cryptocurrency/splashActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/splashActivity.kt
@@ -2,11 +2,17 @@ package com.example.cryptocurrency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class splashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed(object :Runnable{
+            override fun run() {
 
-    }
+            }
+
+        } , 3000)
+        }
 }
