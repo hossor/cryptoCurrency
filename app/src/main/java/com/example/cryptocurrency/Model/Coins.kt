89cfc551918417e7668bc8089e8726c532db8a79
hossor <hossor79@gmail.com>
2020-12-04 12:39:34 +0330

    add model/coins

---
+++ app/src/main/java/com/example/cryptocurrency/Model/Coins.kt
@@ -0,0 +1,30 @@
+package com.example.cryptocurrency.Model
+
+data class Coins(
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val image: String,
+    val current_price: String,
+    val market_cap: String,
+    val market_cap_rank: String,
+    val fully_diluted_valuation: String,
+    val total_volume: String,
+    val high_24h: String,
+    val low_24h: String,
+    val price_change_24h: String,
+    val price_change_percentage_24h: String,
+    val market_cap_change_24h: String,
+    val market_cap_change_percentage_24h: String,
+    val circulating_supply: String,
+    val total_supply: String,
+    val max_supply: String,
+    val ath: String,
+    val ath_change_percentage: String,
+    val ath_date: String,
+    val atl: String,
+    val atl_change_percentage: String,
+    val atl_date: String,
+    val roi: String,
+    val last_updated: String
+)
